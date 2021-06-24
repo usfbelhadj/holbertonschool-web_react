5391de24d938704bb08e3ae1fd9cbd59c1362dd7
usfbelhadj <usf.belhadj@gmail.com>
2021-06-24 19:21:09 +0100

    Push all

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+$.when( $.ready ).then(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+  });
