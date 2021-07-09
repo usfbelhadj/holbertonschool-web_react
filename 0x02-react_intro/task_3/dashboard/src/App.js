ce19a9c53c67f4676dadc178c6511bfe04aaa8f7
usfbelhadj <usf.belhadj@gmail.com>
2021-07-09 23:25:09 +0100

    Push test

--- 0x02-react_intro/task_3/dashboard/src/App.js
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -6,13 +6,13 @@ import {getFullYear, getFooterCopy} from './utils';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} alt="logo" />
         <h1>
         School dashboard
         </h1>
-      </header>
-      <body className="App-body">
+      </div>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
         <div className="form">
         <label for="Email">Email: </label>
@@ -21,12 +21,12 @@ function App() {
         <input type="password"></input>
         <input type="button" value="OK"></input>
         </div>
-      </body>
-      <footer className="App-footer">
+      </div>
+      <div className="App-footer">
         <p>
         Copyright {getFullYear()} - {getFooterCopy(true)}
         </p>
-      </footer>
+      </div>
     </div>
   );
 }
